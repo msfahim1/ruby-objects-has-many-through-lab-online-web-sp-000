d209b7045aa77fe44e0838879b1d87c3afdb4f98
Michael Fahim <michael_fahim@yahoo.com>
2020-01-25 18:45:38 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,24 +1,16 @@
 class Appointment
 
-  attr_accessor :patient, :doctor, :date
-
+  attr_accessor :date, :patient, :doctor
   @@all = []
+
+  def initialize(patient, date, doctor)
+    @patient = patient
+    @date = date
+    @doctor = doctor
+    @@all << self
+  end
 
   def self.all
     @@all
-  end
-
-  def initialize(patient, doctor, date)
-    @patient = patient
-    @doctor = doctor
-    @date = date
-    #Update doctor data fields
-    @doctor.patients << patient
-    @doctor.appointments << self
-    #Update patient data fields
-    @patient.doctors << doctor
-    @patient.appointments << self
-
-    @@all << self
   end
 end
