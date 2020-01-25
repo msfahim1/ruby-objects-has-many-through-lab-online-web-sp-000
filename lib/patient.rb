265f37bfd02cf588b59168ada595d9dee7fe426a
Michael Fahim <michael_fahim@yahoo.com>
2020-01-25 18:44:08 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,6 +1,6 @@
 class Patient
 
-  attr_reader :name
+  attr_accessor :name, :appointment, :doctor
   @@all = []
 
   def initialize(name)
@@ -13,23 +13,15 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select do |appt|
-      appt.patient == self
-    end
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
 
-  def schedule_appointment(doctor, time)
-    Appointment.new(self, doctor, date='tbd', time)
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
   end
 
-  def self.find_by_name(patient_name)
-    self.all.find do |patient|
-      patient.name == patient_name
-    end
-  end
-
-  def find_doc(spec)
-    Doctor.find_by_specialty(spec)
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
   end
 
 end
